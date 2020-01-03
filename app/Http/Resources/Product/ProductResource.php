931c2aa0b4698b1b16086f1bf0d59e37d6101cd1
whiteononon <whiteon0618@gmail.com>
2020-01-03 15:51:12 +0900

    Modified Product Details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,12 +15,32 @@ class ProductResource extends Resource
     public function toArray($request)
     {
         // return parent::toArray($request);
+
+        if ($this->reviews->count() > 0) {
+            $rating = round($this->reviews->sum('star') / $this->reviews->count(), 1);
+        } else {
+            $rating = 'No rating yet';
+        }
+
+        if ($this->stock != 0) {
+            $stock = $this->stock;
+        } else {
+            $stock = 'Out of Stock';
+        }
+
+        $totalPrice = (1 - $this->discount / 100) * $this->price;
+
         return [
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $stock,
             'discount' => $this->discount,
+            'totalPrice' => $totalPrice,
+            'rating' => $rating,
+            'href' => [
+                'reviews' => route('reviews.index', $this->id),
+            ],
         ];
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        factory(Product::class,50)->create();
-        factory(Review::class,300)->create();
+        factory(Product::class, 50)->create();
+        factory(Review::class, 300)->create();
     }
 }
