de22820b5b23bb5d4824cd57ea47e2f67275ba10
whiteononon <whiteon0618@gmail.com>
2020-01-08 06:59:21 +0900

    Product Not Belongs to User

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\App;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
@@ -9,6 +10,10 @@ $factory->define(App\Model\Product::class, function (Faker $faker) {
         'price' => $faker->numberBetween(100,1000),
         'stock' => $faker->randomDigit,
         'discount' => $faker->numberBetween(2,30),
+        'user_id' => function ()
+        {
+          return App\User::all()->random();
+        }
 
     ];
 });
