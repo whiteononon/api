8081f0d2c20cd07013b494b44d8a63aa5b8ab04f
whiteononon <whiteon0618@gmail.com>
2020-01-08 20:28:58 +0900

    Review Create

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,14 +2,18 @@
 
 namespace App\Model;
 
-use Illuminate\Database\Eloquent\Model;
 use App\Model\Product;
+use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    protected $fillable = [
+        'customer',
+        'star',
+        'review',
+    ];
     public function product()
     {
-      return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class);
     }
 }
-  
